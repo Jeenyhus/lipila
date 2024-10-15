694bd9da6d7ce838ccb94943203d2fecd2732715
Jeenyhus <mweembadabwitso@gmail.com>
2024-10-15 15:19:57 +0200

    Added requirements file, showing all the requirements of this project, to be updatedeverytime a package is installed

--- pay/models.py
+++ pay/models.py
@@ -1,3 +1,96 @@
 from django.db import models
 
-# Create your models here.
+"""
+  This contains all models of this POS (Point of Sale) system. and it is used to create the database schema.
+
+  The models are:
+  - Product
+  - Order
+  - OrderItem
+  - Payment
+  - Customer
+  - Transaction
+
+  Each model is a class that inherits from the django.db.models.Model class.
+  Each class represents a table in the database.
+  Each attribute in the class represents a column in the table.
+
+  The class attributes are the fields of the table.
+  The class methods are used to perform operations on the table.
+
+  The class Meta is used to define the table name and other properties of the table.
+"""
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+    class Meta:
+        db_table = 'products'
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey('Customer', on_delete=models.SET_NULL, null=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'orders'
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'order_items'
+
+    def __str__(self):
+        return f'OrderItem {self.id}'
+
+
+class Payment(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    method = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'payments'
+
+    def __str__(self):
+        return f'Payment {self.id}'
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=15)
+
+    class Meta:
+        db_table = 'customers'
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    payment = models.ForeignKey('Payment', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'transactions'
+
+    def __str__(self):
+        return f'Transaction {self.id}'
+
+
