716f95de75e2b007ce799cdc5818daee417c74e7
Jeenyhus <mweembadabwitso@gmail.com>
2024-10-16 11:41:19 +0200

    Created API views to be used to work in react

--- pay/views.py
+++ pay/views.py
@@ -1,5 +1,54 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product, Order, OrderItem, Payment, Customer, Transaction
+from .serializers import ProductSerializer, OrderSerializer, OrderItemSerializer, PaymentSerializer, CustomerSerializer, TransactionSerializer
 
-# Create your views here.
-def index(request):
-    return render(request, 'kumbele/index.html')
+# Product views
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# Order views
+class OrderList(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+# Customer views
+class CustomerList(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+# Payment views
+class PaymentList(generics.ListCreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+class PaymentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+# Transaction views
+class TransactionList(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+
+#OrderItem views
+class OrderItemList (generics.ListCreateAPIView):
+    queryset= OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
